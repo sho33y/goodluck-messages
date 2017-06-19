210213fe4994b37189270dc26435e8437526690e
sho33y <yagishita@buddying.jp>
2017-06-19 18:24:18 +0900

    スタイル組み込み

--- source/src/Controller/GoodlucksController.php
+++ source/src/Controller/GoodlucksController.php
@@ -27,23 +27,6 @@ class GoodlucksController extends AppController
     }
 
     /**
-     * View method
-     *
-     * @param string|null $id Goodluck id.
-     * @return \Cake\Http\Response|void
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $goodluck = $this->Goodlucks->get($id, [
-            'contain' => []
-        ]);
-
-        $this->set('goodluck', $goodluck);
-        $this->set('_serialize', ['goodluck']);
-    }
-
-    /**
      * Add method
      *
      * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
@@ -64,48 +47,4 @@ class GoodlucksController extends AppController
         $this->set('_serialize', ['goodluck']);
     }
 
-    /**
-     * Edit method
-     *
-     * @param string|null $id Goodluck id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $goodluck = $this->Goodlucks->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $goodluck = $this->Goodlucks->patchEntity($goodluck, $this->request->getData());
-            if ($this->Goodlucks->save($goodluck)) {
-                $this->Flash->success(__('The goodluck has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The goodluck could not be saved. Please, try again.'));
-        }
-        $this->set(compact('goodluck'));
-        $this->set('_serialize', ['goodluck']);
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Goodluck id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $goodluck = $this->Goodlucks->get($id);
-        if ($this->Goodlucks->delete($goodluck)) {
-            $this->Flash->success(__('The goodluck has been deleted.'));
-        } else {
-            $this->Flash->error(__('The goodluck could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
-    }
 }
